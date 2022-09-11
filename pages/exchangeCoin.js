fa0d4a5685fdf232c44dffcec886045f7ab2c687
Aakash Garg <aakashgarg1009@gmail.com>
2022-09-11 13:01:36 +0530

    Added the Landing Page

---
+++ pages/exchangeCoin.js
@@ -0,0 +1,14 @@
+
+const exchangeCoin = () => {
+  return (
+    <div>
+        Exchange Coin
+
+    <h1 className="text-3xl font-bold underline">
+         this is the page which is used to exchangeCoin or we can say that is used to convert coin from one to another
+        </h1>
+        </div>
+  )
+}
+
+export default exchangeCoin
---
+++ pages/marketPlace.js
@@ -0,0 +1,17 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/Home.module.css'
+
+const marketPlace = () => {
+  return (
+    <div>marketPlace
+
+<h1 className="text-3xl font-bold underline">
+      Hello world!
+      sab mast chal rha
+    </h1>
+    </div>
+  )
+}
+
+export default marketPlace
