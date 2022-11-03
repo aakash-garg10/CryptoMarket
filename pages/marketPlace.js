f893af1f4eb90cbbaa380728b22b22d8464d0240
Aakash Garg <aakashgarg1009@gmail.com>
2022-11-03 18:35:58 +0530

    added header

--- pages/marketPlace.js
+++ pages/marketPlace.js
@@ -1,15 +1,17 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import Header from '../components/Header'
 
 const marketPlace = () => {
   return (
-    <div>marketPlace
-
-<h1 className="text-3xl font-bold underline">
-     THIS IS THE MARKET PLACE WHERE WE WILL SHOW THE CHARTS AND THE COINS OF THE CRYPTO
-    </h1>
-    </div>
+   <div className="min-h-screen">
+    <Header />
+    <div className="mt-10"></div>
+    {/* Trending */}
+    <div className="mt-20"></div>
+    {/* CoinTable */}
+   </div>
   )
 }
 
